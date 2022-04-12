2947c97366d97656ceea4180cf6e3597bc749648
Siddhant <santoshshrivastava39@gmail.com>
2022-04-12 16:18:05 +0530

    Create Employee Payroll Table

--- payrollservice.sql
+++ payrollservice.sql
@@ -1,3 +1,12 @@
 --UC1-Creating database
 create database payrollservice_119
 
+--UC2-Create table
+create table Employee_payroll
+(
+Id int identity(1,1) primary key,
+Name varchar(20),
+Salary varchar(10),
+StartDate varchar(20)
+)
+select * from Employee_payroll
