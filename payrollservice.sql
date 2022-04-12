e98bca07d71bb25092a21c5dd919553b92f2a21d
Siddhant <santoshshrivastava39@gmail.com>
2022-04-12 16:32:31 +0530

    Insert data into table

--- payrollservice.sql
+++ payrollservice.sql
@@ -10,3 +10,10 @@ Salary varchar(10),
 StartDate varchar(20)
 )
 select * from Employee_payroll
+
+--UC3 Inserting Values into Table
+insert into employee_payroll values ('Rahul','25,000','2018-01-01')
+insert into employee_payroll values ('Sid','45,000','2020-01-01')
+insert into employee_payroll values ('Priyanka','40,000','2021-01-01')
+insert into employee_payroll values ('Akansha','50,000','2022-01-01')
+
