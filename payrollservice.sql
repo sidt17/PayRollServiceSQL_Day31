c5eb196ce4b9cf7c7f59c8f9b237838b6193737c
Siddhant <santoshshrivastava39@gmail.com>
2022-04-12 16:49:04 +0530

    Retrieve salary data as well as particular date

--- payrollservice.sql
+++ payrollservice.sql
@@ -12,10 +12,14 @@ StartDate varchar(20)
 select * from Employee_payroll
 
 --UC3 Inserting Values into Table
-insert into employee_payroll values ('Rahul','25,000','2018-01-01')
-insert into employee_payroll values ('Sid','45,000','2020-01-01')
-insert into employee_payroll values ('Priyanka','40,000','2021-01-01')
-insert into employee_payroll values ('Akansha','50,000','2022-01-01')
+insert into Employee_payroll values ('Rahul','25,000','2018-01-01')
+insert into Employee_payroll values ('Sid','45,000','2020-01-01')
+insert into Employee_payroll values ('Priyanka','40,000','2021-01-01')
+insert into Employee_payroll values ('Akansha','50,000','2022-01-01')
 
 --UC4-Retrievieving employee payroll data
 select * from Employee_payroll
+
+--UC5-Retrive data for particular table
+select Salary from Employee_payroll where Name='Bill';
+select * from Employee_payroll where  StartDate between CAST('2018/01/01' as date) and getdate();
