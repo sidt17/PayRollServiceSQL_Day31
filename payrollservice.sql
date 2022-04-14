30d77f8250b433855097222386180d955a2c160b
Siddhant <santoshshrivastava39@gmail.com>
2022-04-14 16:47:18 +0530

    Make Terissa as part of Sales and Marketing Department

--- payrollservice.sql
+++ payrollservice.sql
@@ -69,7 +69,7 @@ select count(Id) from Employee_payroll;
 select count(Id) as NoOfPeople,Gender from Employee_payroll group by Gender;
 
 --UC8- Extend table to store information like employee phone, address and department
-alter table Employee_payroll add PhoneNo int;
+alter table Employee_payroll add PhoneNo bigint;
 alter table Employee_payroll add Address varchar(250) not null default 'Mumbai';
 alter table Employee_payroll add Department varchar(250) not null default 'Testing';
 select * from Employee_payroll;
@@ -77,3 +77,19 @@ select * from Employee_payroll;
 --UC9-extend employee payroll table to have basic pay, deductions,taxable pay,income tax,net pay
 alter table Employee_payRoll
 add Deduction float,TaxablePay float,IncomeTax float,NetPay float;
+
+--UC10-Adding Department of Terisa as Sales & Marketing Both
+alter table Employee_payroll
+drop column Salary;
+ Update Employee_payroll set 
+ PhoneNo=9992388877 , Department='Sales', Deduction=1450, TaxablePay=500, IncomeTax=1000, NetPay=30000 where name='Rahul'
+ Update employee_payroll set 
+ PhoneNo=9285548899 , Department='Finance', Deduction=1000, TaxablePay=500, IncomeTax=1200, NetPay=38000 where name='Sid'
+ Update employee_payroll set 
+ PhoneNo=7877678544 , Department='Testing', Deduction=1500, TaxablePay=500, IncomeTax=1000, NetPay=45000 where name='Priyanka'
+ Update employee_payroll set 
+ PhoneNo=9956443452 , Department='Marketing', Deduction=1780, TaxablePay=500, IncomeTax=1400, NetPay=28000 where name='Akansha'
+
+ insert into Employee_payroll values('Terissa','2022-03-01','F','Mumbai','Sales',680,500,1000,44000,8787989854)
+ insert into Employee_payroll values('Terissa','2022-02-01','F','Mumbai','Marketing', 800,500,1000,42200,9965875465)
+ select * from Employee_payroll;
